8f1e15acb5512f8be2a13823196dba19601744f0
Helal Uddin -SWE <seoguro1999@gmail.com>
2025-12-24 04:04:30 +0600

    update fronten

--- frontendF/src/utils/axiosInstance.js
+++ frontendF/src/utils/axiosInstance.js
@@ -1,9 +1,16 @@
 import axios from "axios";
 
+const BASE_URL = `${import.meta.env.VITE_API_URL}/api/hotels`;
+
 export const apiJson = axios.create({
-  baseURL: "http://localhost:5000/api/hotels",
+  baseURL: BASE_URL,
+  withCredentials: true,
 });
+
 export const apiForm = axios.create({
-  baseURL: "http://localhost:5000/api/hotels",
-  headers: { "Content-Type": "multipart/form-data" },
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "multipart/form-data",
+  },
+  withCredentials: true,
 });
